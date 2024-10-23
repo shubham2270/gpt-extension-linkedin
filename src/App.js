d87aad656a4dcaa89cfa420b95001e4b8c2648fa
Shubham Kumar <shubham2270@gmail.com>
2024-10-23 11:10:57 +0530

    Basic project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-4'>
+      <h1 className='text-2xl font-bold'>Welcome to My Chrome Extension!</h1>
+      <p className='mt-2 text-gray-700'>
+        Chrome extension built with React and Tailwind CSS.
+      </p>
+      <button className='mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+        Click Me
+      </button>
     </div>
   );
 }
